82cbb9ab901a9514443f76efdd36947df28791c5
admin <admin@admindeiMac.local>
2017-05-31 18:08:56 +0800

    首页改造

---
+++ fxdProduct/fxdProduct/Network/Model/HomeBanner/HomeBannerParamModel.m
@@ -0,0 +1,15 @@
+//
+//  HomeBannerParamModel.m
+//  fxdProduct
+//
+//  Created by admin on 2017/5/31.
+//  Copyright © 2017年 dd. All rights reserved.
+//
+
+#import "HomeBannerParamModel.h"
+
+@implementation HomeBannerParamModel
+
+@synthesize plate_,position_,channel_;
+
+@end
---
+++ fxdProduct/fxdProduct/Network/Model/HomePop/HomePopParam.h
@@ -0,0 +1,18 @@
+//
+//  HomePopParam.h
+//  fxdProduct
+//
+//  Created by admin on 2017/5/31.
+//  Copyright © 2017年 dd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HomePopParam : JSONModel
+
+@property (strong , nonatomic)NSString *channel_;
+@property (strong , nonatomic)NSString *plate_;
+@property (strong , nonatomic)NSString *redpacket_from_;
+
+
+@end
