11e69119499dd33dccc0b4e5bb5e1a667b9acf34
admin <admin@admindeiMac.local>
2017-06-22 17:28:55 +0800

    启动优化

--- fxdProduct/fxdProduct/SetSome.m
+++ fxdProduct/fxdProduct/SetSome.m
@@ -12,42 +12,55 @@
 
 @implementation SetSome
 
++ (SetSome *)shared
+{
+    static dispatch_once_t predicate;
+    static SetSome * _setSome = nil;
+    
+    dispatch_once(&predicate, ^{
+        _setSome = [[SetSome alloc] init];
+    });
+    return _setSome;
+}
+-(void)InitializeAppSet{
+    // 使用umeng分析
+    UMConfigInstance.appKey = UmengKey;
+    //        [MobClick startWithAppkey:UmengKey reportPolicy:BATCH   channelId:@""];
+    [MobClick startWithConfigure:UMConfigInstance];
+    //获取Version，打包时的版本号
+    NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    [MobClick setAppVersion:version];
+    [MobClick setLogEnabled:YES];
+    
+    AFNetworkReachabilityManager *mgr = [AFNetworkReachabilityManager sharedManager];
+    
+    // 2.设置网络状态改变后的处理
+    [mgr setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        // 当网络状态改变了, 就会调用这个block
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown: // 未知网络
+            case AFNetworkReachabilityStatusNotReachable: // 没有网络(断网)
+                DLog(@"未知网络 || 没有网络(断网)");
+                [Utility sharedUtility].networkState = NO;
+                break;
+                
+            case AFNetworkReachabilityStatusReachableViaWWAN: // 手机自带网络
+            case AFNetworkReachabilityStatusReachableViaWiFi: // WIFI
+                DLog(@"手机自带网络 || WIFI");
+                [Utility sharedUtility].networkState = YES;
+                break;
+        }
+    }];
+    
+    // 3.开始监控
+    [mgr startMonitoring];
+    
+}
+
 + (void)load
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        
-        // 使用umeng分析
-        UMConfigInstance.appKey = UmengKey;
-//        [MobClick startWithAppkey:UmengKey reportPolicy:BATCH   channelId:@""];
-        [MobClick startWithConfigure:UMConfigInstance];
-        //获取Version，打包时的版本号
-        NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
-        [MobClick setAppVersion:version];
-        [MobClick setLogEnabled:YES];
-        
-        AFNetworkReachabilityManager *mgr = [AFNetworkReachabilityManager sharedManager];
-        
-        // 2.设置网络状态改变后的处理
-        [mgr setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
-            // 当网络状态改变了, 就会调用这个block
-            switch (status) {
-                case AFNetworkReachabilityStatusUnknown: // 未知网络
-                case AFNetworkReachabilityStatusNotReachable: // 没有网络(断网)
-                    DLog(@"未知网络 || 没有网络(断网)");
-                    [Utility sharedUtility].networkState = NO;
-                    break;
-                    
-                case AFNetworkReachabilityStatusReachableViaWWAN: // 手机自带网络
-                case AFNetworkReachabilityStatusReachableViaWiFi: // WIFI
-                    DLog(@"手机自带网络 || WIFI");
-                    [Utility sharedUtility].networkState = YES;
-                    break;
-            }
-        }];
-        
-        // 3.开始监控
-        [mgr startMonitoring];
         
         NSString *identifyStr = [SSKeychain passwordForService:SERVERNAME account:SERVERNAME];
         if (![identifyStr isEqualToString:@""] && identifyStr) {
@@ -63,14 +76,13 @@
             }
         }
         
-//        [Tool saveUserDefaul:nil Key:Fxd_JUID];
-//        [Tool saveUserDefaul:nil Key:kLoginFlag];
-//        [Tool saveUserDefaul:nil Key:UserName];
+        //        [Tool saveUserDefaul:nil Key:Fxd_JUID];
+        //        [Tool saveUserDefaul:nil Key:kLoginFlag];
+        //        [Tool saveUserDefaul:nil Key:UserName];
         [Utility sharedUtility].userInfo.juid = [Tool getContentWithKey:Fxd_JUID];
         [Utility sharedUtility].userInfo.tokenStr = [Tool getContentWithKey:Fxd_Token];
         [Utility sharedUtility].loginFlage = [[Tool getContentWithKey:kLoginFlag] integerValue];
         [Utility sharedUtility].userInfo.userName = [Tool getContentWithKey:UserName];
     });
 }
-
 @end
