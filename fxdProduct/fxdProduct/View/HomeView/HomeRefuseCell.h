75feb9d973a39e960d47e479785d2d5b3d3fda77
sxp <sxp@sxpdeiMac.local>
2017-07-05 14:11:22 +0800

    首页拒绝导流视图修改

---
+++ fxdProduct/fxdProduct/View/HomeView/HomeRefuseCell.h
@@ -0,0 +1,21 @@
+//
+//  HomeRefuseCell.h
+//  fxdProduct
+//
+//  Created by sxp on 2017/7/5.
+//  Copyright © 2017年 dd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@protocol HomeRefuseCellDelegate <NSObject>
+-(void)clickView:(NSString *)url;
+-(void)moreClick;
+@end
+
+@interface HomeRefuseCell : UITableViewCell<UITableViewDelegate,UITableViewDataSource>
+
++ (instancetype)cellWithTableView:(UITableView *)tableView;
+@property(nonatomic,assign) id<HomeRefuseCellDelegate>delegate;
+@end
