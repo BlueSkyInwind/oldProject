0a0cea2dec3f348706dcf3a6602d4be1620fe481
admin <admin@admindeiMac.local>
2017-07-26 20:57:47 +0800

    代码提交

--- fxdProduct/fxdProduct/Common/GlobalConfig.h
+++ fxdProduct/fxdProduct/Common/GlobalConfig.h
@@ -29,7 +29,6 @@ blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 // 设置三原色
 #define RGBColor(R,G,B,A) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
 
-
 #define _view_width    self.view.frame.size.width
 #define _view_height   self.view.frame.size.height
 #define _k_w           [UIScreen mainScreen].bounds.size.width
