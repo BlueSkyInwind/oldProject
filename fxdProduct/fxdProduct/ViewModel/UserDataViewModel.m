a2506799e74a8f0fa34fc5c5cf0813f7c9f01e10
admin <admin@admindeiMac.local>
2017-09-28 11:05:21 +0800

    update web 交互修改

--- fxdProduct/fxdProduct/ViewModel/UserDataViewModel.m
+++ fxdProduct/fxdProduct/ViewModel/UserDataViewModel.m
@@ -113,8 +113,15 @@
             [[MBPAlertView sharedMBPTextView] showTextOnly:[UIApplication sharedApplication].keyWindow message:faceIDLiveParse.error_message];
         }
     } failure:^(EnumServerStatus status, id object) {
+        NSError * error  = (NSError *)object;
+        NSDictionary *erroInfo = error.userInfo;
+        NSData *data = [erroInfo valueForKey:@"com.alamofire.serialization.response.error.data"];
+        NSString *errorString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", errorString);
+        [self uploadLiveInfo:@"" isSuccess:^(id object) {
+            
+        }];
         //        NSError *error = object;
-        //
         //        [[MBPAlertView sharedMBPTextView] showTextOnly:self.view message:[NSString stringWithFormat:@"Error-%ld",(long)error.code]];
     }];
 }
@@ -129,13 +136,10 @@
     } failure:^(EnumServerStatus status, id object) {
         
     }];
-    
 }
 #pragma  mark - 社保  公积金
-
 /**
  社保
- 
  @param taskid 任务id
  */
 -(void)socialSecurityInfoUpload:(NSString *)taskid{
