d6001228f13a72dba15fe5dd4a36b544f29f282a
admin <admin@admindeiMac.local>
2017-11-10 19:17:28 +0800

    update

--- fxdProduct/fxdProduct/Common/GlobalConfig.m
+++ fxdProduct/fxdProduct/Common/GlobalConfig.m
@@ -37,7 +37,7 @@ NSString * const _main_new_url  =  @"https://h5.faxindai.com:8028/";
 //测试导流 192.168.6.134
 NSString * const _main_url  =   @"http://h5.test.fxds/fxd-esb/esb/";
 NSString * const _agreement_url  =   @"http://h5.test.fxds/fxd-esb/esb/";
-NSString * const _main_new_url  =  @"http://service-apigateway.test.fxds:8005/";
+NSString * const _main_new_url  =  @"http://h5.test.fxds/";
 NSString * const _ValidESB_url  =   @"http://h5.test.fxds/fxd-esb/";
 NSString * const _H5_url    =   @"http://h5.test.fxds/fxd-h5/page/";
 NSString * const _p2P_url = @"http://h5.test.fxds/fxd-esb/p2p/";
