b6b4d42c5a88af9b576cdac172e6b54301d962c9
admin <admin@wyximac.local>
2018-06-07 10:52:33 +0800

    update

---
+++ fxdProduct/fxdProduct/View/PhonerechargeCardService/CardNumAndPasswordCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardNumAndPasswordCell.swift
+//  fxdProduct
+//
+//  Created by admin on 2018/6/7.
+//  Copyright © 2018年 dd. All rights reserved.
+//
+
+import UIKit
+
+class CardNumAndPasswordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ fxdProduct/fxdProduct/View/PhonerechargeCardService/PrepaidCardsInfoCell.swift
@@ -0,0 +1,45 @@
+//
+//  PrepaidCardsInfoCell.swift
+//  fxdProduct
+//
+//  Created by admin on 2018/6/7.
+//  Copyright © 2018年 dd. All rights reserved.
+//
+
+import UIKit
+
+class PrepaidCardsInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+extension PrepaidCardsInfoCell {
+    
+    func  setUpUI()  {
+        
+        
+        
+    }
+}
+
+
+
